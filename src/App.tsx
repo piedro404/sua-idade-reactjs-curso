588499890bb9a4bce57a3685e3f440f177eacc16
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-03-22 16:40:57 -0300

    feat: :art: Create UI for Project Sua Idade

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
-    <div>
-      
+    <div className="container">
+      <h1 className="title">Descubra sua Idade</h1>
+      <form className="form">
+        <label>Digite seu nome?</label>
+        <input
+          className="input"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Ex: Pedro Henrique"
+          required
+        />
+
+        <label>Digite o ano que Nasceu?</label>
+        <input
+          className="input"
+          type="number"
+          name="name"
+          id="name"
+          placeholder="Ex: 2004"
+          required
+        />
+
+        <input className="button" type="submit" value="Calcular" />
+      </form>
+      <p className="result">Pedro Henrique, você tem: 19 anos</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
